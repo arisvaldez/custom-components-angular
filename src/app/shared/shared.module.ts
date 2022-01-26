24aa32538c7c8f50c5c692d7adff57047d787536
aris valdez <samus45@hotmail.es>
2022-01-25 23:46:44 -0400

    tab component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [CustomButtonComponent],
+  declarations: [CustomButtonComponent, TabsComponent, TabComponent],
   imports: [CommonModule, MatButtonModule, FontAwesomeModule],
-  exports: [CustomButtonComponent],
+  exports: [CustomButtonComponent, TabsComponent, TabComponent],
 })
 export class SharedModule {}
---
+++ src/app/shared/tab/tab.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss'],
+})
+export class TabComponent implements OnInit {
+  @Input() tabTitle?: string;
+  active: boolean = false;
+
+  ngOnInit(): void {}
+}
